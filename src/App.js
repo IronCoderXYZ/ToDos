210f99f05317725a0a11147c43e7dd8ba5c3215e
Mitch Hankins <mitchhankins92@gmail.com>
2017-08-17 16:04:59 -0600

    Implemented single root App component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Hi </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
