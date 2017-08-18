4156f4c8d2c05ed712206e0947700ff793ab0281
Mitch Hankins <mitchhankins92@gmail.com>
2017-08-17 18:48:23 -0600

    Added remove task functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
 import { Button, Card, CardSection, Header, Input } from './components/common';
 
 class App extends Component {
   state = { tasks: ['Trash', 'Groceries', 'Taxes'], task: '' }
+
+  completeTask(index) {
+    console.log(this.state.tasks[index]);
+    let tasks = this.state.tasks;
+    tasks = tasks.slice(0, index).concat(tasks.slice(index + 1));
+    this.setState({ tasks });
+  }
 
   addTask(task) {
     const tasks = this.state.tasks.concat([task]);
@@ -14,10 +22,18 @@ class App extends Component {
   }
 
   renderTaskList(tasks) {
-    return tasks.map(task =>
+    return tasks.map((task, index) =>
+      <View>
       <CardSection key={task}>
         <Text> {task} </Text>
+          <TouchableOpacity
+            onPress={() => this.completeTask(index)}
+          >
+            <Text> &#10003; </Text>
+          </TouchableOpacity>
       </CardSection>
+
+      </View>
     );
   }
 
